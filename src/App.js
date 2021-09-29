9605ed1eabebbd234dcede8b34b13619bcfa9602
AmerAbuZaiton123 <amerabuzaiton@outlook.com>
2021-09-29 13:42:23 +0300

    Lab6

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import Form from './component/Form';
+import Location from './component/Location';
+import Error from './component/Error';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+      cityName:'',
+      lat: '',
+      lon: '',
+      show_Results: false,
+      show_Error: false,
+      Mass_Error: '',
+        
+    };
+    }
+
+    FormDataLocation=async(e)=>
+    { e.preventDefault()
+      let cityName1=e.target.value;
+      this.setState({
+        cityName:cityName1
+      })
+    }
+   
+     Submit = async(e) => {
+      e.preventDefault(); 
+        let Url=`https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATION_KEY}&q=${this.state.cityName}&format=json`
+       try{
+            axios.get(Url).then(res=>{
+              let data=res.data[0]
+                 this.setState({
+                 lat:data.lat,
+                 lon:data.lon,
+                 show_Results: true,
+                 show_Error: false, 
+              })
+          console.log(data)
+            }) 
+           }
+      catch{
+      await  this.setState({
+        show_Results: false,
+        show_Error: true 
+      })
+    }
+  
+  }
+  render() {
+    return (
+      <div>
+     <Form FormDataLocation={this.FormDataLocation} Submit={this.Submit} />
+     {this.state.show_Error && <Error show_Error={this.state.show_Error}/>}
+     {this.state.show_Results&&<Location show_Results={this.state.show_Results} cityName={this.state.cityName}  lon={this.state.lon} lat={this.state.lat} />}       
+      </div>
+    )
+  }
 }
-
-export default App;
---
+++ src/component/Error.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Error extends Component {
+    render() {
+        return (
+            <div>
+                <img width={'100%'} src="https://www.google.com/imgres?imgurl=https%3A%2F%2Flookaside.fbsbx.com%2Flookaside%2Fcrawler%2Fmedia%2F%3Fmedia_id%3D103491274677414&imgrefurl=https%3A%2F%2Fm.facebook.com%2Fpages%2FERROR-404%2F103434161349792&tbnid=A37bGVACqCMhRM&vet=12ahUKEwjqhoiRzqPzAhUO7xoKHVOnCF8QMygDegUIARC1AQ..i&docid=v9etk7MKNL-U8M&w=783&h=391&q=error%20404&ved=2ahUKEwjqhoiRzqPzAhUO7xoKHVOnCF8QMygDegUIARC1AQ" alt="" />
+    
+            </div>
+        )
+    }
+}
